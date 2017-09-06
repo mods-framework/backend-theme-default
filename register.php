7ec910a0f3f3423e8230967b6f923a828533733d
Sunel Tr <sunelbe@gmail.com>
2017-09-06 08:10:08 +0530

    Default backend theme

---
+++ register.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'default' => [
+        'name' => 'Default',
+        'type' => 'theme',
+        'area' => 'backend',
+        'path' =>  __DIR__,
+        'parent' => null
+    ]
+];
