f86eeb6a22371206ae7ab1e4946928dae9f2289a
Sunel Tr <sunelbe@gmail.com>
2017-10-14 23:03:44 +0530

    created backend base layout

---
+++ templates/backend/footer.blade.php
@@ -0,0 +1,11 @@
+<script>
+    $(document).ready(function() {
+        $(".dropdown-button").dropdown();
+        $('.tooltipped').tooltip({
+            delay: 50
+        });
+        $(".side-nav-button").sideNav();
+        $('.collapsible').collapsible();
+        $('.modal').modal();
+    });
+</script>
---
+++ templates/backend/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <!-- start header nav-->
+    <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper">
+                <ul class="left">
+                    <li class="hide-on-large-only">
+                        <a href="javascript:void(0);" data-activates="open-left-menu" class="side-nav-button waves-effect waves-block waves-light"> <i class="material-icons">menu</i> </a>
+                    </li>
+                    <li>
+                        <h1 class="logo-wrapper"> <a href="#!" class="brand-logo"> <span class="m">M</span> <span class="o">o</span> <span class="d">d</span> <span class="s">s</span> </a> </h1> 
+                    </li>
+                </ul>
+                 {!! $block->getTopMenu() !!}                
+            </div>
+        </nav>
+    </div>
+    <!-- end header nav-->
+</header>
